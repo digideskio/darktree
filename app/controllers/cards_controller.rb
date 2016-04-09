d624fa337b5ba1d159240f0734af60b9bce3470e
mogulla3 <hs.iarn3@gmail.com>
2016-04-09 17:44:45 +0900

    Order card by id

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,7 +4,7 @@ class CardsController < ApplicationController
   def index
     @cards = Card.status_is(params[:status]).tag_in(params[:tags])
                  .by_query(params[:query]).sort_by(params[:sort])
-                 .by_fav(params[:fav]).page(params[:page]).includes(:tags)
+                 .by_fav(params[:fav]).page(params[:page]).includes(:tags).order(id: :asc)
     @tags = Tag.select(:name)
   end
 
