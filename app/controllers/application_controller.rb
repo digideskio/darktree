7011371d93b8567243dc57c3c43a066cfd2da7b8
mogulla3 <hs.iarn3@gmail.com>
2016-06-04 13:50:46 +0900

    Implemented decks#create, decks#update, decks#destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: :json_request?
+  rescue_from ActionController::ParameterMissing, with: :handle_400
+
+  def handle_400(exception)
+    respond_to do |format|
+      # TODO: format.html
+      format.json { render json: { message: exception.message } }
+    end
+  end
 
   protected
 
--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,6 +1,44 @@
 class DecksController < ApplicationController
+  before_action :set_deck, only: [:update, :destroy]
+
   def index
     @decks = Deck.all.order(id: :desc)
+  end
+
+  def create
+    @deck = Deck.new(deck_params)
+
+    respond_to do |format|
+      if @deck.save
+        format.html { redirect(decks_path, 'Deck was successfully created') }
+        format.json { render json: @deck }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @deck.errors, status: 400 }
+      end
+    end
+  end
+
+  def update
+    redirect_to root_path and return if @deck.nil?
+
+    respond_to do |format|
+      if @deck.update(deck_params)
+        format.html { redirect(decks_path, 'Deck was successfully updated') }
+        format.json { render json: @deck }
+      else
+        format.html { render :edit }
+        format.json { render json: @deck.errors, status: 400 }
+      end
+    end
+  end
+
+  def destroy
+    @deck.destroy
+    respond_to do |format|
+      format.html { redirect(decks_path, 'Deck was successfully deleted') }
+      format.json { render json: nil, status: 204 }
+    end
   end
 
   def search
@@ -11,5 +49,15 @@ class DecksController < ApplicationController
             end
 
     render json: decks
+  end
+
+  private
+
+  def set_deck
+    @deck = Deck.find_by(id: params[:id])
+  end
+
+  def deck_params
+    params.require(:deck).permit(:name)
   end
 end
